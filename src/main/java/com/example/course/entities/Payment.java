9730be498bfe38d560d4ba88220eab20d975c30f
João Paulo <joaowais@gmail.com>
2024-08-04 20:15:56 -0300

    Payment, one-to-one association

---
+++ src/main/java/com/example/course/entities/Payment.java
@@ -0,0 +1,72 @@
+package com.example.course.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment () {
+
+    }
+
+    public Payment(Long id, Instant moment, com.example.course.entities.Order order) {
+        this.id = id;
+        this.moment = moment;
+        order = order;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public com.example.course.entities.Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(com.example.course.entities.Order order) {
+        order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Payment payment = (Payment) o;
+        return Objects.equals(getId(), payment.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+}
